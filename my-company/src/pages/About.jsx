9bf3a1f0e9eed866706534c644b39470438732fb
Faith Kalamshe <magagulachari@gmail.com>
2025-09-08 21:59:47 +0200

    Add my-company React project with 4 pages and routing

---
+++ my-company/src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>About Faith Charisma Enterprises</h1>
+      <p>
+        Founded by Faith Charisma Kalamshe, our company has been providing
+        top-notch services since 2025. We specialize in technology consulting,
+        market analysis, and product development, helping businesses achieve their goals.
+      </p>
+    </div>
+  );
+}
+
+export default About;
