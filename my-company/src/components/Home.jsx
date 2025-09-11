ebd06e59c98487dd521c82d19f32a1015ddb228b
Faith Kalamshe <magagulachari@gmail.com>
2025-09-12 01:40:23 +0200

    Fix: move all page components to src/components and update imports in App.jsx

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to Faith Charisma Enterprises</h1>
+      <p>
+        We are dedicated to delivering excellence in all our services.
+        Our mission is to help businesses grow and thrive with innovative solutions.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
+
