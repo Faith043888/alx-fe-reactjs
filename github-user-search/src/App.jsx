5be6c819a00c1284ad876e30bb21c076f2f32867
Faith Kalamshe <magagulachari@gmail.com>
2025-09-27 01:32:01 +0200

    Update project files

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,10 @@
-import "./App.css";
-import Search from "./components/Search";
-
 function App() {
   return (
-    <div className="App">
-      <h1>GitHub User Search</h1>
-      <Search />
+    <div className="bg-blue-500 text-white p-6 rounded-lg">
+      <h1 className="text-2xl font-bold">Hello Tailwind!</h1>
     </div>
-  );
+  )
 }
 
 export default App;
+
